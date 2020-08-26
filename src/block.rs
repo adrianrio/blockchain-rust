b5f39854a1ab2e2034e98bc6626f7bb2751b91d7
adrianrio <48291070+adrianrio@users.noreply.github.com>
2020-08-26 11:12:11 +0900

    tes

--- src/block.rs
+++ src/block.rs
@@ -4,70 +4,33 @@ use super::*;
 pub struct Block {
     pub index: u32,
     pub timestamp: u128,
-    pub hash: Hash,
-    pub prev_block_hash: Hash,
+    pub hash: BlockHash, 
+    pub prev_block_hash: BlockHash,
     pub nonce: u64,
-    pub transactions: Vec<Transaction>,
-    pub difficulty: u128,
+    pub payload: String, 
 }
 
 impl Debug for Block {
     fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block[{}]: {} at: {} with: {} nonce: {}",
+        write!(f, "Block[{}]: {} at: {} with: {}",
             &self.index,
             &hex::encode(&self.hash),
             &self.timestamp,
-            &self.transactions.len(),
+            &self.transactions,
             &self.nonce,
         )
     }
 }
 
 impl Block {
-    pub fn new (index: u32, timestamp: u128, prev_block_hash: Hash, transactions: Vec<Transaction>, difficulty: u128) -> Self {
-        Block {
+    pub fn new (index: u32, timestamp: u128, prev_block_hash: BlockHash, 
+        nonce: u64, payload: String) -> Self{
             index,
             timestamp,
-            hash: vec![0; 32],
+            hash: vec![0; 32], 
             prev_block_hash,
-            nonce: 0,
-            transactions,
-            difficulty,
+            nonce,
+            payload, 
         }
     }
 
-    pub fn mine (&mut self) {
-        for nonce_attempt in 0..(u64::max_value()) {
-            self.nonce = nonce_attempt;
-            let hash = self.hash();
-            if check_difficulty(&hash, self.difficulty) {
-                self.hash = hash;
-                return;
-            }
-        }
-    }
-}
-
-impl Hashable for Block {
-    fn bytes (&self) -> Vec<u8> {
-        let mut bytes = vec![];
-
-        bytes.extend(&u32_bytes(&self.index));
-        bytes.extend(&u128_bytes(&self.timestamp));
-        bytes.extend(&self.prev_block_hash);
-        bytes.extend(&u64_bytes(&self.nonce));
-        bytes.extend(
-            self.transactions
-                .iter()
-                .flat_map(|transaction| transaction.bytes())
-                .collect::<Vec<u8>>()
-        );
-        bytes.extend(&u128_bytes(&self.difficulty));
-
-        bytes
-    }
-}
-
-pub fn check_difficulty (hash: &Hash, difficulty: u128) -> bool {
-    difficulty > difficulty_bytes_as_u128(&hash)
-}
