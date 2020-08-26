1d27ba3aa18fb4f0693e1131a98930b0cc922f47
adrianrio <48291070+adrianrio@users.noreply.github.com>
2020-08-26 12:34:25 +0900

    tes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use blockchainlib::*;
 
 fn main() {
-    // let block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned()); //first block
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned()); //first block
 
-    println!("Hello");
+    println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
+
+    block.hash = h;
+
+    println!("{:?}", &block);
 }
